391a1f1d32e096510a3f04e7c41db15b70ef9c9e
bishtntl <bishtntl04@gmail.com>
2024-04-23 01:59:27 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,21 @@
 import React from "react";
-// import image1 from "./images/logo.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../Assetes/images/mackinlay_logo.png";
 import "../Css/header.css";
 
 const Header = () => {
+  const navi = useNavigate();
   return (
     <div>
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container customheader">
           <a class="navbar-brand" href="/">
-          <img
-                    src={logo}width="100px;" alt="not-found" onClick={() => ("/")}
-                 />
+            <img
+              src={logo}
+              width="100px;"
+              alt="not-found"
+              onClick={() => "/"}
+            />
           </a>
           <button
             class="navbar-toggler"
@@ -28,35 +31,36 @@ const Header = () => {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-4 mb-2 mb-lg-0">
               <li class="nav-item">
-              <NavLink to="/" className="nav-link">
-                     Home
-                     </NavLink>
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
               </li>
               <li class="nav-item">
-              <NavLink to="/discover" className="nav-link">
-                         Discover
-                    </NavLink>
+                <NavLink to="/discover" className="nav-link">
+                  Discover
+                </NavLink>
               </li>
 
               <li class="nav-item">
-              <NavLink to="/service" className="nav-link">
-                        Service
-                       </NavLink>
+                <NavLink to="/service" className="nav-link">
+                  Service
+                </NavLink>
               </li>
               <li class="nav-item">
-              <NavLink to="/mission" className="nav-link">
-                        Mission
-                     </NavLink>
+                <NavLink to="/mission" className="nav-link">
+                  Mission
+                </NavLink>
               </li>
               <li class="nav-item">
-              <NavLink to="/contact" className="nav-link">
-                       Contact
-                     </NavLink>
+                <NavLink to="/contact" className="nav-link">
+                  Contact
+                </NavLink>
               </li>
-             
             </ul>
             <form class="d-flex">
-             <NavLink to="/signup" style={{ color:"white"}}> <button className="sign-in-btn">Sign In</button></NavLink>
+              <button className="sign-in-btn" onClick={() => navi("/signup")}>
+                Sign In
+              </button>
             </form>
           </div>
         </div>
